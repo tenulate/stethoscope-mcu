c95a7407fbc5b2742a4a0834591f741d00a018e6
Marko Milicevic <tenulate@gmail.com>
2015-07-13 10:11:13 +1000

    Transfer ENTIRE WORD from ADC to DMA (not just single BYTE) - Fix blink rate not changing predicatbly

--- DMA.c
+++ DMA.c
@@ -5,8 +5,8 @@
 #include <stdint.h>
 
 // Allocate memory for Buffer A and Buffer B in DMA memory
-uint16_t BufferA[NUM_SAMPLES] __attribute__((space(dma), aligned(64)));
-uint16_t BufferB[NUM_SAMPLES] __attribute__((space(dma), aligned(64)));
+uint16_t BufferA[NUM_SAMPLES] __attribute__((space(dma), aligned(32)));
+uint16_t BufferB[NUM_SAMPLES] __attribute__((space(dma), aligned(32)));
 
 /*******************************************************************************
  * Initializes DMA Channel 4 to retrieve data from ADC in continuous ping-pong 
@@ -15,7 +15,7 @@ uint16_t BufferB[NUM_SAMPLES] __attribute__((space(dma), aligned(64)));
 void InitDMA4(void)
 {
     DMA_ADC_DATA_DIR = PERIPHERAL_TO_DMA;       // TX from ADC to DMA
-    DMA_ADC_DATA_TX_SIZE = DMA_BYTE;            // transfer byte at a time
+    DMA_ADC_DATA_TX_SIZE = DMA_WORD;            // transfer 16 bits of data
     DMA_ADC_INTERRUPT_CONDITION = WHEN_ALL_DATA_MOVED;
     DMA_ADC_ADDRESS_MODE = PERIPHERAL_INDIRECT; // ADC takes care of addressing
     DMA_ADC_CH_MODE = CONTINUOUS_PING_PONG;     // 2 buffers to write to
