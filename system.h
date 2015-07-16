2b2d2a1a243d2ac6e8c1fdda09d0a214fe5bceee
Marko Milicevic <tenulate@gmail.com>
2015-07-16 11:31:05 +1000

    Add comments to PLL parameters explaining M, N1, N2

--- system.h
+++ system.h
@@ -2,13 +2,10 @@
 /* System Level #define Macros                                                */
 /******************************************************************************/
 
-// clock settings
-//#define M 65                        // PLL multiplying factor from Fref to Fvco
-//#define N1 3                        // PLL division from Fin to Fref
-//#define N2 2                        // PLL division factor from Fvco to Fosc
-#define M 65
-#define N1 3
-#define N2 2
+// clock settings                 
+#define M 65                        // PLL multiplying factor from Fref to Fvco
+#define N1 3                        // PLL division from Fin to Fref
+#define N2 2                        // PLL division factor from Fvco to Fosc
 #define SYS_FREQ    79841667L       // Fosc = Fin*M/N1/N2, Fin = 7.37MHz
 #define FCY         SYS_FREQ/2      // Instruction clock frequency
  
