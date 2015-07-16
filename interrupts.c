16487c8630c95f2f0f6b91c95346fb302be62141
Marko Milicevic <tenulate@gmail.com>
2015-07-16 11:52:27 +1000

    Remove #include C30 libraries from user.c, traps.c, interrupts.c

--- interrupts.c
+++ interrupts.c
@@ -3,16 +3,7 @@
 /******************************************************************************/
 
 /* Device header file */
-#if defined(__XC16__)
-    #include <xc.h>
-#elif defined(__C30__)
-    #if defined(__dsPIC33E__)
-    	#include <p33Exxxx.h>
-    #elif defined(__dsPIC33F__)
-    	#include <p33Fxxxx.h>
-    #endif
-#endif
-
+#include <xc.h>
 #include <stdint.h>        /* Includes uint16_t definition   */
 
 /******************************************************************************/
