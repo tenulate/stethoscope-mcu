ec60c22f0faa652e7bef73ea9598f59bb59ae595
Marko Milicevic <tenulate@gmail.com>
2015-07-19 15:42:20 +1000

    Align user.h #definitions Remove UART #define

--- user.h
+++ user.h
@@ -6,32 +6,28 @@
 #define USER_H
 
 // Map MCU pins to logical names
-#define LED_PIN LATBbits.LATB12
-#define LED_PIN_DIR TRISBbits.TRISB12
+#define LED_PIN             LATBbits.LATB12
+#define LED_PIN_DIR         TRISBbits.TRISB12
 
-#define ADC_PIN_DIR TRISAbits.TRISA0
-#define ADC_PIN AD1PCFGLbits.PCFG0
+#define ADC_PIN_DIR         TRISAbits.TRISA0
+#define ADC_PIN             AD1PCFGLbits.PCFG0
 
 // Digital/Analog settings
-#define ANALOG 0
-#define DIGITAL 1
+#define ANALOG              0
+#define DIGITAL             1
 
 /* Application specific user parameters used in user.c may go here */
-#define OUTPUT 0
-#define INPUT 1
-#define OFF 0
-#define ON 1
-#define INFINITE_LOOP 1
-
-// UART configs
-#define RP7_O RPOR3bits.RP7R
-#define U1TX 0b00011
+#define OUTPUT              0
+#define INPUT               1
+#define OFF                 0
+#define ON                  1
+#define INFINITE_LOOP       1
 
 // LED config
-#define TIME_TO_WAIT 150
-#define LED_OFF (LED_PIN = OFF)
-#define LED_ON (LED_PIN = ON)
-#define MIN_TIME 25             // min time to flash LED
+#define TIME_TO_WAIT        150
+#define LED_OFF             (LED_PIN = OFF)
+#define LED_ON              (LED_PIN = ON)
+#define MIN_TIME            25             // min time to flash LED
 
 /******************************************************************************/
 /* User Function Prototypes                                                   */
