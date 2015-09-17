9c98f5a3d6bea7efe51440d3dcaf516beed4bc9c
Marko Milicevic <tenulate@gmail.com>
2015-09-17 15:26:47 +1000

    Update LED pin comments Turn off peripheral pin select (PPS)

--- user.c
+++ user.c
@@ -8,14 +8,14 @@
 void initApp(void)
 {
     /* Setup analog functionality and port direction */
-    LED_PIN_TYPE = DIGITAL; // Pin 18 is digital
-    LED_PIN_DIR = OUTPUT;   // Pin 18 output
-    ADC_PIN_DIR = INPUT;    // Pin 2 input
-    ADC_PIN_TYPE = ANALOG;  // Pin 2 analog
+    LED_PIN_TYPE = DIGITAL; // Pin 18 (RB9) is digital
+    LED_PIN_DIR = OUTPUT;   // Pin 18 (RB9) output
+    ADC_PIN_DIR = INPUT;    // Pin 2 (AN0) input
+    ADC_PIN_TYPE = ANALOG;  // Pin 2 (AN0) analog
     
     // Route UART1 TX through RP7 (pin 16)
     // PPSUnLock;
-    PPSOutput(OUT_FN_PPS_U1TX, OUT_PIN_PPS_RP7);
+    // PPSOutput(OUT_FN_PPS_U1TX, OUT_PIN_PPS_RP7);
     // PPSLock;
 }
 
